7f20ccbb444348121e5c12925292f1050bbd2f6d
X. An <xdan2930@hotmail.com>
2017-04-03 16:43:09 -0500

    Second program updated c++ and its Makefile.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 using namespace std;
 
 int sum(int, int);
+int product(int, int);
 
 int main(){
 	cout << "Hello World" << endl;
+	cout << sum(3, 4) << endl;
+	cout << product(3, 4) << endl;
 }
 
 int sum(int a, int b){
 	return a+b;
+}
+
+int product(int a, int b){
+	return a*b;
 }
